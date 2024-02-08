26aab2bb7cacc6c83d06eb4fde25503db78509d8
tusharbmandge <157129289+tusharbmandge@users.noreply.github.com>
2024-02-08 18:41:25 +0530

    Time: 7 ms (29.56%) | Memory: 9.1 MB (25.01%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,29 +1,10 @@
 class Solution {
 public:
-    int decimalToBinary(int n)
-    {
-        if(n==0)
-        {
-            return 0;
-        }
-        int count=0;
-        while(n>0)
-        {
-            int temp=n%2;
-            if(temp==1)
-            {
-                count++;
-            }
-            n=n/2;
-        }
-        return count;
-    }
     vector<int> countBits(int n) {
-        vector<int> ans;
-        for(int i=0;i<=n;i++)
+        vector<int> ans(n+1,0);
+        for(int i=1;i<=n;i++)
         {
-           int temp= decimalToBinary(i);
-           ans.push_back(temp);
+            ans[i]=ans[i/2]+i%2;
         }
         return ans;
     }
